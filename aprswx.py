ef76fa3a10aaba02c0a1d048fdc29e3dd49f5018
Roland Ulrich <ulrich@init7.net>
2025-02-10 14:29:56 +0100

    rework for python3

--- aprswx.py
+++ aprswx.py
@@ -1,27 +1,35 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import json
 import requests
 import datetime
 import time
 from requests.auth import HTTPBasicAuth
+import importlib.util
 
-exec(open("credentials.py").read())
-exec(open("stations.py").read())
-exec(open("rubrics.py").read())
+# Moderne Art, Konfigurationsdateien zu importieren
+def load_config(filename):
+    spec = importlib.util.spec_from_file_location("config", filename)
+    config = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config)
+    return config
+
+# Konfigurationsdateien laden
+credentials = load_config("credentials.py")
+stations = load_config("stations.py")
+rubrics = load_config("rubrics.py")
 
 # Split array into smaller portions
-def chunks(l,n):
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
+def chunks(lst, n):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 # check response from DAPNET
 def check_response(resp):
     if resp.status_code != 201:
         error = json.loads(resp.text)
-        print (error)
-        print ("Error at", station[0])
-        print ('POST /news/ {}'.format(resp.status_code))
+        print(error)
+        print(f"Error at {station[0]}")
+        print(f'POST /news/ {resp.status_code}')
 
 # Send data from wx station to dapnet
 def send_DAPNET(entry, station):
@@ -33,71 +41,75 @@ def send_DAPNET(entry, station):
     msgtime = datetime.datetime.fromtimestamp(ts_epoch).strftime('%H%M')
 
     # now it's time for building up the message itself
-    msg = msgtime + "z " + station["callsign"] + "/" + station["qth"] + ": "
+    msg = f"{msgtime}z {station['callsign']}/{station['qth']}: "
 
-    # WX-data in try-blocks, because not every WX-station delivers all data
-    try:
-        temperature = entry["temp"] + "C "
-        msg+=temperature
-    except:
-        pass
+    # Dictionary für optionale WX-Daten
+    wx_data = {
+        'temp': lambda: f"{entry['temp']}C ",
+        'wind': lambda: f"w: {entry['wind_speed']}m/s at {entry['wind_direction']}deg ",
+        'humidity': lambda: f"h: {entry['humidity']}% ",
+        'rain': lambda: f"rain: {entry['rain_1h']}mm/h"
+    }
 
-    try:
-        wind = "w: " + entry["wind_speed"]+ "m/s at " + entry["wind_direction"] + "deg "
-        msg+=wind
-    except:
-        pass
-
-    try:
-        humidity = "h: " + entry["humidity"] + "% "
-        msg+=humidity
-    except:
-        pass
-
-    try:
-        rain = "rain: " + entry["rain_1h"] + "mm/h"
-        msg+=rain
-    except:
-        pass
+    # Elegantere Behandlung der optionalen Daten
+    for key, formatter in wx_data.items():
+        try:
+            msg += formatter()
+        except (KeyError, TypeError):
+            continue
 
     # preparing the post-message
-    post={ "rubricName": station["rubric"], "text": msg, "number": station["slot"] }
-    print (post)
+    post = {
+        "rubricName": station["rubric"],
+        "text": msg,
+        "number": station["slot"]
+    }
+    print(post)
 
     # and sending it to DAPNET
-    resp = requests.post('https://hampager.de/api/news/', json=post, auth=HTTPBasicAuth(dapnetuser, dapnetpasswd))
+    resp = requests.post(
+        'https://hampager.de/api/news/',
+        json=post,
+        auth=HTTPBasicAuth(credentials.dapnetuser, credentials.dapnetpasswd)
+    )
     check_response(resp)
 
     # this is a work-around for mirroring messages into another rubric 
     if station["rubric"] == "aprswx-dl-bw":
-        post={ "rubricName": "hochrhein", "text": msg, "number": station["slot"] }
-        print (post)
-        resp = requests.post('https://hampager.de/api/news/', json=post, auth=HTTPBasicAuth(dapnetuser, dapnetpasswd))
+        post["rubricName"] = "hochrhein"
+        print(post)
+        resp = requests.post(
+            'https://hampager.de/api/news/',
+            json=post,
+            auth=HTTPBasicAuth(credentials.dapnetuser, credentials.dapnetpasswd)
+        )
         check_response(resp)
 
 # let the party begin!
-selected_stations=""
-querystring=""
+def main():
+    selected_stations = []
+    
+    # Stationen nach Rubriken filtern
+    selected_stations = [
+        station["callsign"]
+        for station in stations
+        if any(rubric['rubric'] == station['rubric'] for rubric in rubrics)
+    ]
+    
+    # In Chunks verarbeiten
+    for station_chunk in chunks(selected_stations, 20):
+        querystring = ",".join(station_chunk)
+        response = requests.get(
+            f"https://api.aprs.fi/api/get?name={querystring}&what=wx&apikey={credentials.aprsapikey}&format=json"
+        )
+        wx = response.json()
+        
+        for entry in wx["entries"]:
+            station = next(
+                item for item in stations 
+                if item["callsign"] == entry["name"]
+            )
+            send_DAPNET(entry, station)
 
-# the following has to be done for each WX-station in the list
-for i in range(0,len(stations)):
-
-    # Build a string with all stations selected by rubrics
-    if [ stations for rubric in rubrics if stations[i]['rubric'] == rubric['rubric'] ]:
-        selected_stations+=stations[i]["callsign"]+","
-
-# due to query-rate-limits on aprs.fi and maximum of 20 stations per
-# query we have to work in portions
-for line in list(chunks(selected_stations.split(","),20)):
-    querystring = ",".join(line)
-    # fetching data from aprs.fi
-    response = requests.get("https://api.aprs.fi/api/get?name="+querystring+"&what=wx&apikey="+aprsapikey+"&format=json")
-    wx = json.loads(response.text)
-    entries = (wx["entries"])
-
-    # lets do something good with the stuff we got
-    for entry in entries:
-        #station = (item for item in stations if item["callsign"] == entry["name"]).next()
-        station = next(item for item in stations if item["callsign"] == entry["name"])
-        # calling send-function above
-        send_DAPNET(entry, station)
+if __name__ == "__main__":
+    main()
